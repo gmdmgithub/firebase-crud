103e54d7f0de228fde3a4b77393990ed2fa02d34
gmdmgithub <gmika@interia.pl>
2018-11-12 02:16:57 +0100

    async Pipe is used instead subscribe and unsubscribe method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,19 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
-import { environment } from '../environments/environment';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnDestroy {
-  projects: any[];
+export class AppComponent {
+  
+  projects$;
   title = 'Hi first app with firebase backend';
-  subscription: Subscription;
 
   constructor(db: AngularFireDatabase) {
-    console.log(environment.firebase);
+    // console.log(environment.firebase);
 
-    this.subscription = db
-      .list('/projects')
-      .valueChanges()
-      .subscribe(projects => {
-        this.projects = projects;
-        console.log(projects);
-      });
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.projects$ = db.list('/projects').valueChanges();
   }
 }
