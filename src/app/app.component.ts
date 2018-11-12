dfb7e6bec2369a254711b57b91945f4cc523525c
gmdmgithub <gmika@interia.pl>
2018-11-12 02:10:58 +0100

    getting data from firebase
    first subscribe and unsubscribe method is useed - unsubscribe is neccessary to prevent memory leeks
    unsubscribe is in onDestroy method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnDestroy } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { environment } from '../environments/environment';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'firebase-warm-up';
+export class AppComponent implements OnDestroy {
+  projects: any[];
+  title = 'Hi first app with firebase backend';
+  subscription: Subscription;
+
+  constructor(db: AngularFireDatabase) {
+    console.log(environment.firebase);
+
+    this.subscription = db
+      .list('/projects')
+      .valueChanges()
+      .subscribe(projects => {
+        this.projects = projects;
+        console.log(projects);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
