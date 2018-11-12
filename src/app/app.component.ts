efc48a78a14fd9d18333e20eac07bcf173836239
gmdmgithub <gmika@interia.pl>
2018-11-12 22:34:12 +0100

    add (push) new item to firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,33 @@
-import { Component } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
-import { Subscription, Observable } from 'rxjs';
+import { Component } from "@angular/core";
+import { AngularFireDatabase, AngularFireList } from "angularfire2/database";
+import { Subscription, Observable } from "rxjs";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  
-  projects$;
+  projects$: Observable<any[]>;
   project$;
-  title = 'Hi first app with firebase backend';
+  title = "Hi first app with firebase backend";
+  projectCollection: AngularFireList<any>;
 
   constructor(db: AngularFireDatabase) {
     // console.log(environment.firebase);
 
-    this.projects$ = db.list('/projects').valueChanges();
-    this.project$ = db.object('projects/test2').valueChanges();
+    this.projects$ = db.list("/projects").valueChanges();
+    this.project$ = db.object("projects/test2").valueChanges();
+    this.projectCollection = db.list("projects");
+  }
+
+  addProject(project: HTMLInputElement) {
+    const proj = {
+      name: project.value,
+      description: project.value + " description"
+    };
+    this.projectCollection
+      .push(proj)
+      .then(_ => console.log("success", proj));
+    project.value = "";
   }
 }
